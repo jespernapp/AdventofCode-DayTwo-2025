f97d47b5a98cea22017da02bd724e2bd5b84f962
jespernapp <jesperooo@live.se>
2025-12-02 19:14:12 +0100

    add CsvReader

---
+++ CsvReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode_DayTwo_2025
+{
+    public class CsvReader
+    {
+        public static List<string> ReadCsv(string filePath)
+        {
+            try
+            {
+                var ranges = File.ReadAllText(filePath).Split(',').ToList();
+                return ranges;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception($"Error reading CSV file: {ex.Message}");
+            }
+        }
+    }
+}
